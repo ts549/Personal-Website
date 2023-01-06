ac26f494c5b4b95992c3258b6a4027bfd6c0e0b4
ts549 <sun955@purdue.edu>
2023-01-06 04:20:14 -0500

    shadow hover + minor updates

--- client/src/components/Overview/Overview.js
+++ client/src/components/Overview/Overview.js
@@ -11,17 +11,35 @@ function Overview() {
                     About Me <br/>
                     My name is Tao Sun and I'm a Junior studying Computer Science as well as Mathematics at Purdue University. I'm <br/> <a href="/AboutMe">Read More...</a>
                 </div>
+                <div className="ShadowCover">
+                    <span className="ShadowText">About Me</span>
+                    <a href="/AboutMe" className="arrow_wrapper">
+                        <button className="arrow" />
+                    </a>
+                </div>
             </div>
             <div id="ExpSect" className="section">
                 <div className="Text">
                     Text Text Text Text <br/> <a href="/Experiences">Read More...</a>
                 </div>
                 <div className="Pic"></div>
+                <div className="ShadowCover">
+                    <span className="ShadowText">Experiences</span>
+                    <a href="/Experiences" className="arrow_wrapper">
+                        <button className="arrow" />
+                    </a>
+                </div>
             </div>
             <div id="ProjSect" className="section">
                 <div className="Pic"></div>
                 <div className="Text">
                     Text Text Text Text <br/> <a href="/Projects">Read More...</a>
+                </div>
+                <div className="ShadowCover">
+                    <span className="ShadowText">Projects</span>
+                    <a href="/Projects" className="arrow_wrapper">
+                        <button className="arrow" />
+                    </a>
                 </div>
             </div>
             <div id="SkillsSect" className="section">
@@ -29,6 +47,12 @@ function Overview() {
                     Text Text Text Text <br/> <a href="AboutMe">Read More...</a>
                 </div>
                 <div className="Pic"></div>
+                <div className="ShadowCover">
+                    <span className="ShadowText">Skills</span>
+                    <a href="/Skills" className="arrow_wrapper">
+                        <button className="arrow" />
+                    </a>
+                </div>
             </div>
         </div>
     );
